6527a59a18bdbf03c965069d65fa10b92aaf7590
Can Güney Aksakalli <cgaksakalli@gmail.com>
2020-01-23 18:16:08 +0100

    feat: get locations as dict helper (#4)

--- openlr/utils.py
+++ openlr/utils.py
@@ -21,3 +21,39 @@ def j_round(float_num):
     """java like rounding for complying with the OpenLR java: 2.5 -> 3"""
     num = decimal.Decimal(float_num).to_integral_value(rounding=decimal.ROUND_HALF_UP)
     return int(num)
+
+
+def get_lonlat_list(location):
+    """Helper to return a list of lonlat tuples of coordinates in a location"""
+    lonlat_list = []
+    if hasattr(location, "lon") and hasattr(location, "lat"):
+        lonlat_list.append((location.lon, location.lat))
+    if hasattr(location, "point"):
+        lonlat_list.append((location.point.lon, location.point.lat))
+    if hasattr(location, "points"):
+        lonlat_list.extend([(p.lon, p.lat) for p in location.points])
+    if hasattr(location, "corners"):
+        lonlat_list.extend([(c.lon, c.lat) for c in location.corners])
+    if hasattr(location, "lowerLeft") and hasattr(location, "upperRight"):
+        lonlat_list.append((location.lowerLeft.lon, location.lowerLeft.lat))
+        lonlat_list.append((location.upperRight.lon, location.upperRight.lat))
+    return lonlat_list
+
+
+def _namedtuple_to_dict(obj):
+    if hasattr(obj, "_fields"):
+        dict_obj = {}
+        for field in obj._fields:
+            dict_obj[field] = _namedtuple_to_dict(getattr(obj, field))
+        return dict_obj
+    if hasattr(obj, "__iter__"):
+        return [_namedtuple_to_dict(o) for o in obj]
+    return obj
+
+
+def get_dict(location):
+    """Helper to convert location object to dict"""
+    return {
+        "type": location.__class__.__name__,
+        "properties": _namedtuple_to_dict(location),
+    }
--- tests/test_others.py
+++ tests/test_others.py
@@ -24,3 +24,9 @@ class TestXMLFormat(OpenlrBaseTestCase):
         for name, _, location in LOCATIONS:
             lonlat_list = openlr.get_lonlat_list(location)
             self.assertNotEqual(len(lonlat_list), 0, msg="no latlon for " + name)
+
+
+def test_get_dict(self):
+    for name, _, location in LOCATIONS:
+        location_dict = openlr.get_dict(location)
+        self.assertIsNotNone(location_dict)
